14f08ebd295a26e022082637d85d03ec9aee86bd
Anna <cameaann@gmail.com>
2025-10-31 19:03:13 +0200

    added jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true
+    }]
+  },
+  testMatch: [
+    '**/__tests__/**/*.(ts|tsx)',
+    '**/*.(test|spec).(ts|tsx)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.(ts|tsx)',
+    '!src/**/*.d.ts',
+  ],
+};
