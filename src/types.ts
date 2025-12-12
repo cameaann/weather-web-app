c0dc493cbb2e157c5331f76d06873bdf9431b529
Anna <cameaann@gmail.com>
2025-12-12 19:57:16 +0200

    added HourlyForecast interface

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,16 @@ export interface DailyForecast {
   weather_code: number;
 }
 
+export interface HourlyForecast {
+  time: string;
+  date: string;
+  temperature_2m: number;
+  relative_humidity_2m: number;
+  wind_speed_10m: number;
+  apparent_temperature: number;
+  weather_code: number;
+}
+
 export interface WeatherData {
   city: string;
   country?: string;
@@ -17,12 +27,9 @@ export interface WeatherData {
     relative_humidity_2m?: number;
     precipitation?: number | undefined;
   };
-  hourly: {
-    temperature_2m: number[];
-    relative_humidity_2m: number[];
-    wind_speed_10m: number[];
-  };
+  hourly:  HourlyForecast[];
   daily: DailyForecast[];
+  hourlyByDay: Record<string, HourlyForecast[]>;
 }
 
 export type WeatherContextType = {
